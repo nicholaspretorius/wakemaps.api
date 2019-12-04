fbe6826f45ac1b81ea1de4549c9559ded5634a54
Nicholas Pretorius <nicholaspretorius@gmail.com>
2019-12-04 13:02:10 +0200

    chore: Add prod

--- services/web/entrypoint.sh
+++ services/web/entrypoint.sh
@@ -11,6 +11,12 @@ then
     echo "PostgreSQL started"
 fi
 
-python manage.py create_db
+if [ "$FLASK_ENV" = "development" ]
+then
+    echo "Creating the database tables..."
+    python manage.py create_db
+    python manage.py seed_db
+    echo "Tables created"
+fi
 
 exec "$@"
