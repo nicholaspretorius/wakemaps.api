3470fa1dd49255d57fdf2ebece85fafa356295cd
Nicholas Pretorius <nicholaspretorius@gmail.com>
2019-12-05 18:29:38 +0200

    chore: Cleanup

--- services/web/project/__init__.py
+++ services/web/project/__init__.py
@@ -7,32 +7,6 @@ db = SQLAlchemy(app)
 
 from . import models  # noqa
 
-# class Wakepark(db.Model):
-#     __tablename__ = 'wakeparks'
-
-#     id = db.Column(db.Integer, primary_key=True)
-#     name = db.Column(db.String(255), unique=True, nullable=False)
-
-#     def __init__(self, name):
-#         self.name = name
-
-#     def insert(self):
-#         db.session.add(self)
-#         db.session.commit()
-
-#     def update(self):
-#         db.session.commit()
-
-#     def delete(self):
-#         db.session.delete(self)
-#         db.session.commit()
-
-#     def format(self):
-#         return {
-#             'id': self.id,
-#             'name': self.name
-#         }
-
 
 @app.route('/')
 def hello():
